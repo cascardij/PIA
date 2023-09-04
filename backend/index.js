161ddca8798585cb977ed2a566b71f3331ccc7ea
João Cascardi <seu-emailjoao.cascardi@aluno.ifsp.edu>
2023-09-04 09:19:09 -0300

    implementação bdd

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 const express=require('express')
 const app = express()
 app.listen(9000, () => console.log("OK"));
+
+const mysql = require('mysql/promise')
+const connection = mysql.createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: ''
+})
+
 app.get('/', (req,res)=>{
     res.send ("Cascardi");
 })
+
